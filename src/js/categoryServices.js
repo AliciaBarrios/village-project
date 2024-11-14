a2be0e2fc7d03d27e45c6b9cc0de64abe13f03e7
Alicia Barrios Garcia <abarriosga@uoc.edu>
2024-11-14 17:53:45 +0100

    eliminando instalaciones

--- src/js/categoryServices.js
+++ src/js/categoryServices.js
@@ -1,12 +1,12 @@
 //Obtención data para las categorías
-// import eventos from '../eventos.json';
-import instalaciones from '../instalaciones.json';
+import eventos from '../eventos.json';
+// import instalaciones from '../instalaciones.json';
 import restaurantes from '../restaurantes.json';
 import rutas from '../rutas.json';
 
 const data = {
-    // 'eventos': eventos,
-    'instalaciones': instalaciones,
+    'eventos': eventos,
+    // 'instalaciones': instalaciones,
     'rutas': rutas,
     'restaurantes': restaurantes
 }
