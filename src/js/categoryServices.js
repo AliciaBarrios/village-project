5e2ddb8cac01dcb4e6dfd34fd4c40cd08522496b
Alicia Barrios Garcia <abarriosga@uoc.edu>
2024-11-15 08:06:10 +0100

    unificando JS

--- src/js/categoryServices.js
+++ src/js/categoryServices.js
@@ -74,3 +74,84 @@ function toggleOptions(categoryId, event) {
 
 window.toggleOptions = toggleOptions;
 
+function getCategoryItems(category) {
+    return data[category];
+}
+function getById(id,categoryItems) {
+    const selectedInterestPoint = categoryItems.find(item => item.id === parseInt(id));
+    return selectedInterestPoint;
+}
+
+function loadInterestPoint(interestPoint) {
+    document.getElementById('breadcrumbs').innerHTML = `
+    <a href="./categorias.html" class="nav-link">Categorías</a> > <a href="#" class="nav-link active-link">${interestPoint.title}</a>
+    `;
+    document.getElementById('title').textContent = interestPoint.title;
+    document.getElementById('img').src = images[interestPoint.img];
+    document.getElementById('direction').innerHTML = `<p><i class="fas fa-map-marker-alt"></i> ${interestPoint.direction}</p>`;
+
+    // Dividir descripción en parrafos
+    const description = interestPoint.description;
+    const descriptionContainer = document.getElementById('description');
+
+    for(let paragraph of description) {
+        const paragraphElement = document.createElement('p');
+        paragraphElement.textContent = paragraph;
+        descriptionContainer.appendChild(paragraphElement);
+    }
+}
+
+function loadInterestPointByCategory(interestPoint, categoryUrl) {
+    // Añadimos detalles según categría
+    if (categoryUrl === 'restaurantes' || categoryUrl === 'instalaciones') {
+        document.getElementById('phone').innerHTML = `<p><i class="fas fa-phone-alt"></i> ${interestPoint.phone}</p>`;
+
+        const scheduleDiv = document.getElementById('schedule');
+        const table = document.createElement('table');
+        
+        const headerRow = table.insertRow();
+        headerRow.innerHTML = "<th>Dia</th><th>Horari</th>";
+    
+        Object.entries(interestPoint.schedule).forEach(([day, hours]) => {
+            const row = table.insertRow();
+            row.insertCell().textContent = day.charAt(0).toUpperCase() + day.slice(1);
+            row.insertCell().textContent = hours;
+        });
+    
+        scheduleDiv.appendChild(table);
+    } else if (categoryUrl === 'rutas') {
+        document.getElementById('route-details').innerHTML = `
+            <p><i class="fas fa-hiking"></i> ${interestPoint.distance}</p>
+            <p><i class="fas fa-mountain"></i> ${interestPoint.elevation}</p>
+            <p><i class="fas fa-clock"></i> ${interestPoint.time}</p>
+        `;
+    } else if (categoryUrl === 'eventos') {
+        document.getElementById('event-details').innerHTML = `
+            <p><i class="fas fa-calendar-alt"></i> ${interestPoint.date}</p>
+            <p><i class="fas fa-clock"></i> ${interestPoint.hour}</p>
+        `;
+    }
+}
+function loadOtherInterestPoints(categoryItems,categoryUrl,idUrl) {
+    const container = document.getElementById('related-links');
+    container.innerHTML = categoryItems
+    .filter(item => item.id.toString() !== idUrl)
+    .map(item => `<li><a href="./detalle?categoria=${categoryUrl}&id=${item.id}">${item.title}</a></li>`)
+    .join('');
+}
+
+export function loadUrlInterestPoint() {
+    const params = new URLSearchParams(window.location.search);
+    const idUrl = params.get("id");
+    const categoryUrl = params.get("categoria");
+
+    const categoryItems = getCategoryItems(categoryUrl);
+    const interestPoint = getById(idUrl, categoryItems);
+    
+    loadInterestPoint(interestPoint);
+    loadInterestPointByCategory(interestPoint, categoryUrl);
+    loadOtherInterestPoints(categoryItems,categoryUrl, idUrl);
+}
+
+loadUrlInterestPoint();
+
--- src/js/interestPointServices.js
+++ src/js/interestPointServices.js
@@ -11,120 +11,120 @@
 //     'restaurantes': restaurantes
 // }
 
-import data from "../data.json";
+// // import data from "../data.json";
 
-//Importación imágenes
-import imgCanVirgili from '../assets/img_can_virgili.jpg';
-import imgCasal from '../assets/img_casal.jpeg';
-import imgFontMoreu from '../assets/img_font_moreu.jpg';
-import imgAndreu from '../assets/img_andreu.jpg';
-import imgFogonsXeremell from '../assets/img_fogons_xeremell.jpg';
-import imgDolmen from '../assets/img_dolmen.jpg';
-import imgOlzinelles from '../assets/img_olzinelles.jpg';
-import imgPagesia from '../assets/img_pagesia.png';
-import imgBaronia from '../assets/img_baronia.jpg';
-import imgVallgorguina from '../assets/img_vallgorguina.jpeg';
-import imgTio from '../assets/img_tio.jpg';
-import imgTourdera from '../assets/img_tourdera.png';
-import imgMuseu from '../assets/img_museu.jpg';
-import imgDinamic from '../assets/img_dinamic.jpg';
-import imgPoliesportiu from '../assets/img_poliesportiu.jpg';
+// //Importación imágenes
+// import imgCanVirgili from '../assets/img_can_virgili.jpg';
+// import imgCasal from '../assets/img_casal.jpeg';
+// import imgFontMoreu from '../assets/img_font_moreu.jpg';
+// import imgAndreu from '../assets/img_andreu.jpg';
+// import imgFogonsXeremell from '../assets/img_fogons_xeremell.jpg';
+// import imgDolmen from '../assets/img_dolmen.jpg';
+// import imgOlzinelles from '../assets/img_olzinelles.jpg';
+// import imgPagesia from '../assets/img_pagesia.png';
+// import imgBaronia from '../assets/img_baronia.jpg';
+// import imgVallgorguina from '../assets/img_vallgorguina.jpeg';
+// import imgTio from '../assets/img_tio.jpg';
+// import imgTourdera from '../assets/img_tourdera.png';
+// import imgMuseu from '../assets/img_museu.jpg';
+// import imgDinamic from '../assets/img_dinamic.jpg';
+// import imgPoliesportiu from '../assets/img_poliesportiu.jpg';
 
-export const images = {
-    'imgCanVirgili': imgCanVirgili,
-    'imgCasal': imgCasal,
-    'imgFontMoreu': imgFontMoreu,
-    'imgAndreu': imgAndreu,
-    'imgFogonsXeremell': imgFogonsXeremell,
-    'imgDolmen': imgDolmen,
-    'imgOlzinelles': imgOlzinelles,
-    'imgPagesia': imgPagesia,
-    'imgBaronia': imgBaronia,
-    'imgVallgorguina': imgVallgorguina,
-    'imgTio': imgTio,
-    'imgTourdera': imgTourdera,
-    'imgMuseu': imgMuseu,
-    'imgDinamic': imgDinamic,
-    'imgPoliesportiu': imgPoliesportiu
-}
+// export const images = {
+//     'imgCanVirgili': imgCanVirgili,
+//     'imgCasal': imgCasal,
+//     'imgFontMoreu': imgFontMoreu,
+//     'imgAndreu': imgAndreu,
+//     'imgFogonsXeremell': imgFogonsXeremell,
+//     'imgDolmen': imgDolmen,
+//     'imgOlzinelles': imgOlzinelles,
+//     'imgPagesia': imgPagesia,
+//     'imgBaronia': imgBaronia,
+//     'imgVallgorguina': imgVallgorguina,
+//     'imgTio': imgTio,
+//     'imgTourdera': imgTourdera,
+//     'imgMuseu': imgMuseu,
+//     'imgDinamic': imgDinamic,
+//     'imgPoliesportiu': imgPoliesportiu
+// }
 
-function getCategoryItems(category) {
-    return data[category];
-}
-function getById(id,categoryItems) {
-    const selectedInterestPoint = categoryItems.find(item => item.id === parseInt(id));
-    return selectedInterestPoint;
-}
+// function getCategoryItems(category) {
+//     return data[category];
+// }
+// function getById(id,categoryItems) {
+//     const selectedInterestPoint = categoryItems.find(item => item.id === parseInt(id));
+//     return selectedInterestPoint;
+// }
 
-function loadInterestPoint(interestPoint) {
-    document.getElementById('breadcrumbs').innerHTML = `
-    <a href="./categorias.html" class="nav-link">Categorías</a> > <a href="#" class="nav-link active-link">${interestPoint.title}</a>
-    `;
-    document.getElementById('title').textContent = interestPoint.title;
-    document.getElementById('img').src = images[interestPoint.img];
-    document.getElementById('direction').innerHTML = `<p><i class="fas fa-map-marker-alt"></i> ${interestPoint.direction}</p>`;
+// function loadInterestPoint(interestPoint) {
+//     document.getElementById('breadcrumbs').innerHTML = `
+//     <a href="./categorias.html" class="nav-link">Categorías</a> > <a href="#" class="nav-link active-link">${interestPoint.title}</a>
+//     `;
+//     document.getElementById('title').textContent = interestPoint.title;
+//     document.getElementById('img').src = images[interestPoint.img];
+//     document.getElementById('direction').innerHTML = `<p><i class="fas fa-map-marker-alt"></i> ${interestPoint.direction}</p>`;
 
-    // Dividir descripción en parrafos
-    const description = interestPoint.description;
-    const descriptionContainer = document.getElementById('description');
+//     // Dividir descripción en parrafos
+//     const description = interestPoint.description;
+//     const descriptionContainer = document.getElementById('description');
 
-    for(let paragraph of description) {
-        const paragraphElement = document.createElement('p');
-        paragraphElement.textContent = paragraph;
-        descriptionContainer.appendChild(paragraphElement);
-    }
-}
+//     for(let paragraph of description) {
+//         const paragraphElement = document.createElement('p');
+//         paragraphElement.textContent = paragraph;
+//         descriptionContainer.appendChild(paragraphElement);
+//     }
+// }
 
-function loadInterestPointByCategory(interestPoint, categoryUrl) {
-    // Añadimos detalles según categría
-    if (categoryUrl === 'restaurantes' || categoryUrl === 'instalaciones') {
-        document.getElementById('phone').innerHTML = `<p><i class="fas fa-phone-alt"></i> ${interestPoint.phone}</p>`;
+// function loadInterestPointByCategory(interestPoint, categoryUrl) {
+//     // Añadimos detalles según categría
+//     if (categoryUrl === 'restaurantes' || categoryUrl === 'instalaciones') {
+//         document.getElementById('phone').innerHTML = `<p><i class="fas fa-phone-alt"></i> ${interestPoint.phone}</p>`;
 
-        const scheduleDiv = document.getElementById('schedule');
-        const table = document.createElement('table');
+//         const scheduleDiv = document.getElementById('schedule');
+//         const table = document.createElement('table');
         
-        const headerRow = table.insertRow();
-        headerRow.innerHTML = "<th>Dia</th><th>Horari</th>";
+//         const headerRow = table.insertRow();
+//         headerRow.innerHTML = "<th>Dia</th><th>Horari</th>";
     
-        Object.entries(interestPoint.schedule).forEach(([day, hours]) => {
-            const row = table.insertRow();
-            row.insertCell().textContent = day.charAt(0).toUpperCase() + day.slice(1);
-            row.insertCell().textContent = hours;
-        });
+//         Object.entries(interestPoint.schedule).forEach(([day, hours]) => {
+//             const row = table.insertRow();
+//             row.insertCell().textContent = day.charAt(0).toUpperCase() + day.slice(1);
+//             row.insertCell().textContent = hours;
+//         });
     
-        scheduleDiv.appendChild(table);
-    } else if (categoryUrl === 'rutas') {
-        document.getElementById('route-details').innerHTML = `
-            <p><i class="fas fa-hiking"></i> ${interestPoint.distance}</p>
-            <p><i class="fas fa-mountain"></i> ${interestPoint.elevation}</p>
-            <p><i class="fas fa-clock"></i> ${interestPoint.time}</p>
-        `;
-    } else if (categoryUrl === 'eventos') {
-        document.getElementById('event-details').innerHTML = `
-            <p><i class="fas fa-calendar-alt"></i> ${interestPoint.date}</p>
-            <p><i class="fas fa-clock"></i> ${interestPoint.hour}</p>
-        `;
-    }
-}
-function loadOtherInterestPoints(categoryItems,categoryUrl,idUrl) {
-    const container = document.getElementById('related-links');
-    container.innerHTML = categoryItems
-    .filter(item => item.id.toString() !== idUrl)
-    .map(item => `<li><a href="./detalle?categoria=${categoryUrl}&id=${item.id}">${item.title}</a></li>`)
-    .join('');
-}
+//         scheduleDiv.appendChild(table);
+//     } else if (categoryUrl === 'rutas') {
+//         document.getElementById('route-details').innerHTML = `
+//             <p><i class="fas fa-hiking"></i> ${interestPoint.distance}</p>
+//             <p><i class="fas fa-mountain"></i> ${interestPoint.elevation}</p>
+//             <p><i class="fas fa-clock"></i> ${interestPoint.time}</p>
+//         `;
+//     } else if (categoryUrl === 'eventos') {
+//         document.getElementById('event-details').innerHTML = `
+//             <p><i class="fas fa-calendar-alt"></i> ${interestPoint.date}</p>
+//             <p><i class="fas fa-clock"></i> ${interestPoint.hour}</p>
+//         `;
+//     }
+// }
+// function loadOtherInterestPoints(categoryItems,categoryUrl,idUrl) {
+//     const container = document.getElementById('related-links');
+//     container.innerHTML = categoryItems
+//     .filter(item => item.id.toString() !== idUrl)
+//     .map(item => `<li><a href="./detalle?categoria=${categoryUrl}&id=${item.id}">${item.title}</a></li>`)
+//     .join('');
+// }
 
-export function loadUrlInterestPoint() {
-    const params = new URLSearchParams(window.location.search);
-    const idUrl = params.get("id");
-    const categoryUrl = params.get("categoria");
+// export function loadUrlInterestPoint() {
+//     const params = new URLSearchParams(window.location.search);
+//     const idUrl = params.get("id");
+//     const categoryUrl = params.get("categoria");
 
-    const categoryItems = getCategoryItems(categoryUrl);
-    const interestPoint = getById(idUrl, categoryItems);
+//     const categoryItems = getCategoryItems(categoryUrl);
+//     const interestPoint = getById(idUrl, categoryItems);
     
-    loadInterestPoint(interestPoint);
-    loadInterestPointByCategory(interestPoint, categoryUrl);
-    loadOtherInterestPoints(categoryItems,categoryUrl, idUrl);
-}
+//     loadInterestPoint(interestPoint);
+//     loadInterestPointByCategory(interestPoint, categoryUrl);
+//     loadOtherInterestPoints(categoryItems,categoryUrl, idUrl);
+// }
 
-loadUrlInterestPoint();
+// loadUrlInterestPoint();
